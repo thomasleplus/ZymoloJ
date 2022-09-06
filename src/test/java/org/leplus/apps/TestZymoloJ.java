a392fde2b0c35ab14b45ea2998f0cbbb7c358a03
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-09-05 18:08:04 -0700

    Test

---
+++ src/test/java/org/leplus/apps/TestZymoloJ.java
@@ -0,0 +1,20 @@
+package org.leplus.apps;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author thomas
+ *
+ */
+class TestZymoloJ {
+
+    @Test
+    void test() {
+        ZymoloJ.main(null);
+    }
+
+}
